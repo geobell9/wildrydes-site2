d4739b3f2c29d815f2e00b264acd1e834deb1293
geobell9 <71470349+geobell9@users.noreply.github.com>
2021-03-29 19:54:17 -0400

    Update config.js

--- wildrydes-site/js/config.js
+++ wildrydes-site/js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_KPX6p23Gn', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '24upj2tc1465j5ubr24vcd2jfp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: ' us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
